6ab2074beb83d91f5a497095f4bd5251401e3d9e
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-06-20 00:00:42 +0200

    Adding some features, changing logic

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-function randomNumber() {
-    return Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var image1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", image1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var image2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute("src", image2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector(".container h2").innerHTML = "Player 1 wins!"
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector(".container h2").innerHTML = "Player 2 wins!"
+} else {
+    document.querySelector(".container h2").innerHTML = "It's a DRAW!"
 }
-
-function randomizeLeft() {
-    var number = randomNumber();
-    document.querySelector(".dice .img1").setAttribute("src", "images/dice" + number + ".png");
-    return number;
-}
-
-function randomizeRight() {
-    var number = randomNumber();
-    document.querySelector(".dice .img2").setAttribute("src", "images/dice" + number + ".png");
-    return number; 
-}
-
-function getValues() {
-    var first = randomizeLeft();
-    var second = randomizeRight();
-    return [first, second];
-}
-
-var [first, second] = getValues();
-
-function whoIsTheWinner() {
-    if (first > second) {
-        document.querySelector(".container h1").innerHTML = "Player 1 Wins!";
-    } else if (first < second) {
-        document.querySelector(".container h1").innerHTML = "Player 2 Wins!";
-    } else {
-        document.querySelector(".container h1").innerHTML = "DRAW!";
-    }
-}
-
-whoIsTheWinner();
